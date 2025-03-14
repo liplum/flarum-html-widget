72c398b4caf482f8ea53709cb041eb3e9236abc6
Liplum <Li_plum@outlook.com>
2025-03-14 13:23:24 +0800

    added less

--- extend.php
+++ extend.php
@@ -4,4 +4,20 @@ namespace Liplum\HtmlWidget;
 
 use Flarum\Extend;
 
-return [];
+return [
+  (new Extend\Frontend('forum'))
+    ->js(__DIR__ . '/js/dist/forum.js')
+    ->css(__DIR__ . '/less/forum.less'),
+
+  (new Extend\Frontend('admin'))
+    ->js(__DIR__ . '/js/dist/admin.js')
+    ->css(__DIR__ . '/less/admin.less'),
+
+  new Extend\Locales(__DIR__ . '/locale'),
+
+  (new Extend\Settings)
+    ->serializeToForum(
+      'liplum-html-widget.innerHtml',
+      'liplum-html-widget.innerHtml'
+    ),
+];
