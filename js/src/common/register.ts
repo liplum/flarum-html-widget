3a1bcc45ccfda6b85daefd7ec9033015c192873a
Liplum <Li_plum@outlook.com>
2025-03-15 15:24:14 +0800

    impl widget

---
+++ js/src/common/register.ts
@@ -0,0 +1,17 @@
+import Application from 'flarum/common/Application';
+import Widgets from 'flarum/extensions/afrux-forum-widgets-core/common/extend/Widgets';
+
+import HtmlWidget from './components/widget';
+
+export default function (app: Application) {
+  new Widgets()
+    .add({
+      key: 'liplum-html-widget',
+      component: HtmlWidget,
+      isDisabled: false,
+      isUnique: true,
+      placement: 'end',
+      position: 2,
+    })
+    .extend(app, 'liplum-html-widget');
+}
---
+++ js/src/forum/index.ts
@@ -0,0 +1,6 @@
+import app from 'flarum/common/app';
+import registerWidget from '../common/register';
+
+app.initializers.add('liplum/html-widget', () => {
+  registerWidget(app);
+});
