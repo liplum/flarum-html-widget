144f2b6a788b2a2c9bac0adc3974bd1723edbf82
Liplum <Li_plum@outlook.com>
2025-03-16 05:21:40 +0800

    style: refactor HtmlWidget to use centralized extension name for attributes

--- js/src/common/components/widget.tsx
+++ js/src/common/components/widget.tsx
@@ -1,5 +1,6 @@
 import app from 'flarum/common/app';
 import Widget, { WidgetAttrs } from 'flarum/extensions/afrux-forum-widgets-core/common/components/Widget';
+import { extName } from '../../r';
 
 interface HtmlWidgetAttrs extends WidgetAttrs {
 
@@ -13,21 +14,21 @@ export default class HtmlWidget extends Widget<HtmlWidgetAttrs> {
   }
 
   icon(): string {
-    const icon = app.forum.attribute<string>('liplum-html-widget.icon');
+    const icon = app.forum.attribute<string>(`${extName}.icon`)
     return icon
   }
 
   title(): string {
-    const title = app.forum.attribute<string>('liplum-html-widget.title');
+    const title = app.forum.attribute<string>(`${extName}.title`)
     return title
   }
 
   content() {
-    return <div id={htmlId}></div>;
+    return <div id={htmlId}></div>
   }
 
   oncreate(vnode): void {
-    const innerHtml = app.forum.attribute<string>('liplum-html-widget.innerHtml');
+    const innerHtml = app.forum.attribute<string>(`${extName}.innerHtml`)
     const e = document.getElementById(htmlId)
     if (e) {
       e.innerHTML = innerHtml
